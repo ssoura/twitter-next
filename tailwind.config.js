aebb92e55fae4d5a84b6ff74c454065e4cdead39
Sourabh Shukla <34943727+ssoura@users.noreply.github.com>
2023-03-18 23:43:33 +0530

    update next tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        White: "#e7e9ea",
-        Blue: "#308CD8",
-        Border: "#2f3336",
-        LightGray: "#71767b",
-        DarkGray: "#17181C",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
