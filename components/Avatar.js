4d520ef94f3ba01cb236d83b9768957954f8e1fc
Sourabh Shukla <34943727+ssoura@users.noreply.github.com>
2023-03-18 23:41:05 +0530

    add ts

--- components/Avatar.js
+++
@@ -1,9 +0,0 @@
-export default function Avatar({ src, big, onChange, editable = false }) {
-  const widthClass = big ? "w-24" : "w-12";
-
-  return (
-    <div className="rounded-full overflow-hidden">
-      <img src={src} />
-    </div>
-  );
-}
